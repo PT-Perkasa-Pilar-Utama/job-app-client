8af8c29f81afec169afe6609ea56274fae634da2
Awal Ariansyah <awalariansyah7@gmail.com>
2025-05-15 15:22:45 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# job-app-client
+
+Simple discord bot as mcp client to be used for MCP demo.
+
+- `job-app-rest`: https://github.com/PT-Perkasa-Pilar-Utama/job-app-rest
+- `job-app-mcp`: https://github.com/PT-Perkasa-Pilar-Utama/job-app-mcp
---
+++ env.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  TOKEN: z.string().nonempty({ message: "TOKEN cannot be empty" }),
+  CLIENT_ID: z.string().nonempty({ message: "CLIENT_ID cannot be empty" }),
+  JOB_APP_MCP_URL: z
+    .string()
+    .url({ message: "JOB_APP_API_URL must be a valid URL" })
+    .nonempty({ message: "JOB_APP_API_URL cannot be empty" }),
+});
+
+export type Env = z.infer<typeof EnvSchema>;
+
+const result = EnvSchema.safeParse(Bun.env);
+if (!result.success) {
+  console.error("❌ Invalid environment variables:");
+  console.error(JSON.stringify(result.error.format(), null, 2));
+  process.exit(1);
+}
+
+const env: Env = result.data;
+export default env!;
